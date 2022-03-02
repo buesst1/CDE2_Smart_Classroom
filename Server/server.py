2185b4180b9e595e4c1fd9f02e759483ea889530
buesst1 <91325328+buesst1@users.noreply.github.com>
2022-03-02 10:58:41 +0100

    update

--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,14 @@
 import socket
 import ssl
+import os
+
 
 HOST = "localhost"
 PORT = 443
+dirname = os.path.dirname(__file__)
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain('certificate.crt', 'certificate.key')
+context.load_cert_chain(dirname + r'\SSL\certificate.crt', dirname + r'\SSL\certificate.key')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     sock.bind((HOST, PORT))
